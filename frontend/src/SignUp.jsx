dfc29e87452035b0d19b3964ae18abfa50ad7ada
Connor Chen <cjc353@cornell.edu>
2022-09-16 18:37:49 -0400

    Set up frontend

---
+++ frontend/src/SignUp.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import { Link } from "react-router-dom";
+
+function SignUp() {
+  return (
+    <div>
+      Sign Up
+      <Link to="/">Sign In</Link>
+    </div>
+  );
+}
+
+export default SignUp;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
