55aa0c14ca4de08c02c43f60a9735d3451377d88
Sng Hong <sng_hong@outlook.com>
2022-09-17 04:29:30 -0400

    fixed leaderboardD

--- frontend/src/Social.jsx
+++ frontend/src/Social.jsx
@@ -2,11 +2,12 @@ import "./index.css";
 import SideBar from './SideBar'
 import Chat from './Chat'
 import NavBar from "./NavBar";
+import SocialLeaderboard from "./SocialLeaderboard"
 function Social() {
   return (
     <div>
     <NavBar />
     <SideBar />
-      
+    <SocialLeaderboard />
     </div>);}
 export default Social;
