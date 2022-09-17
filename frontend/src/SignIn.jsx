9915363fa22db47fb0ca712d543369a2bd80add2
jakef2001 <jakeford2001@gmail.com>
2022-09-16 21:30:33 -0400

    Update signin to include navbar

--- frontend/src/SignIn.jsx
+++ frontend/src/SignIn.jsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import { Link } from "react-router-dom";
 import SideBar from './SideBar'
+import NavBar from './NavBar'
+
 function SignIn() {
   return (
     
-    
     <div>
+          <NavBar />
       Sign In
       
       
